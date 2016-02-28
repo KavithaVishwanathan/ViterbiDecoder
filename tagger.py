cd2d94934a1f2d1bd0a8181d4a2d291d61c64e97
KavithaVishwanathan <kavivisu1612@gmail.com>
2016-02-27 21:35:25 -0500

    find tags

--- tagger.py
+++ tagger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import numpy as np
 from collections import defaultdict
 
 def StateCount(currpos,word,prevpos, wordcout_dict):
@@ -13,7 +14,7 @@ def StateCount(currpos,word,prevpos, wordcout_dict):
 	key4 = prevpos + " to " + currpos
 	wordcout_dict[key4] += 1
 
-def trainWSJCorpus:
+def trainWSJCorpus():
 	#read the training pos file
 	posfile = open("WSJ_02-21.pos", 'r')
 	prevpos = "start"
@@ -30,4 +31,46 @@ def trainWSJCorpus:
 			prevpos = "start"
 	return wordcout_dict
 
+def FindTags():
+	posfile = open("WSJ_02-21.pos", 'r')
+	tags = {}
+	for line in posfile:
+		wordpos = line.split("\t")
+		if len(wordpos) > 1:
+			word = wordpos[0]
+			tag = wordpos[1].strip("\n")
+			tags.setdefault(word,[]).append(tag)
+	#to remove duplicates from list
+	tag_dict = {}
+	for k,v in tags.items():
+		tag_dict[k] = list(set(v))
+	return tag_dict
+
+def FindStates(tokenList):
+	tag_dict = FindTags()
+	stateList = []
+	for token in tokenList:
+		stateList.append(tag_dict[token])
+	return stateList
+
+def obs():
+	finallist = []
+	lineList = []
+	posfile = open("WSJ_24.words", 'r')
+	for line in posfile:
+		if len(line) > 1:
+			lineList.append(line.strip("\n"))
+		else:
+			finallist.append(lineList)
+			lineList = []
+	return finallist
+
+def viterbi():
+	obslist = obs()
+	for tokenList in obslist:
+		stateList = FindStates(tokenList)
+		#np.zeros((len(stateList)),len(tokenList))
+	print  stateList
+
+viterbi()	
 
